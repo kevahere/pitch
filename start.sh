5efb1fd6d9226472d68ea14f2d17af0be044098d
kevahere <kevahere@gmail.com>
2018-10-12 10:54:26 +0300

    modified start.sh

---
+++ start.sh
@@ -0,0 +1,7 @@
+export MAIL_USERNAME=snomoran@gmail.com
+export MAIL_PASSWORD=Snofalls123
+
+export MOVIE_API_KEY='1da453f27393ac3b85881f5f866c58fd'
+export SECRET_KEY='movie'
+
+python3.6 manage.py server
