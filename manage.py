1faafbca770f004e8e7013ca9b9f12908ca4bc13
kevahere <kevahere@gmail.com>
2018-10-12 19:22:30 +0300

    fixed tables

--- manage.py
+++ manage.py
@@ -1,7 +1,7 @@
 from app import create_app,db
 from flask_script import Manager,Server
 from flask_migrate import Migrate,MigrateCommand
-#from app.models import User,Role, Review
+from app.models import User,Pitch, Comment, Category
 
 # Creating app instance
 app = create_app('development')
@@ -20,9 +20,9 @@ def test():
     tests = unittest.TestLoader().discover('tests')
     unittest.TextTestRunner(verbosity=2).run(tests)
 
-# @manager.shell
-# def make_shell_context():
-#     return dict(app = app,db = db,User = User, Role = Role )
+@manager.shell
+def make_shell_context():
+    return dict(app = app,db = db,User = User, Pitch = Pitch, Comment = Comment,Category = Category )
 
 if __name__ == '__main__':
     manager.run()
