276a0a87c5bb6fcd786a3dc1ec35d58ecd6886ba
kevahere <kevahere@gmail.com>
2018-10-12 19:26:40 +0300

    git cache cleared

--- start.sh
+++
@@ -1,7 +0,0 @@
-export MAIL_USERNAME=snomoran@gmail.com
-export MAIL_PASSWORD=Snofalls123
-
-export MOVIE_API_KEY='1da453f27393ac3b85881f5f866c58fd'
-export SECRET_KEY='movie'
-
-python3.6 manage.py server
